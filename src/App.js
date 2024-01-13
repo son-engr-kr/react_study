c9db882591210a238d75b850816bfeb81c771c4b
son_zephyrus_duo <son.engr.kr@gmail.com>
2024-01-14 01:39:01 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '대구 치과 맛집';//서버에서 데이터 가져왔다 가정
   // document.querySelector('h4').innerHTML = post;//원래라면 이렇게 했을 것
   let idTest = "id-test";
+
+
+  let [stateVar,setStateVar] = useState('대구 치과 추천');
+
   return (
     <div className="App">
       <div className="my-first-css-class">
