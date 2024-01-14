0d67f79eec224337dc53058e7361cefd452e67ec
son_zephyrus_duo <son.engr.kr@gmail.com>
2024-01-14 19:08:17 +0900

    css

--- src/App.js
+++ src/App.js
@@ -37,6 +37,13 @@ function App() {
           setArrayStateVar(arrayCopy);
          }}>{ arrayStateVar[1] }</h4>
       </div>
+
+
+      <div className = "modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
     </div>
   );
 }
